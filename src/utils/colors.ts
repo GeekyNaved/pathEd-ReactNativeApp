6f9db694db023fbc84d7312b09df08d55e096b5b
GeekyNaved <navedahmed040@gmail.com>
2025-04-07 12:07:47 +0530

    updating theme of app from orange to green

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,7 +1,7 @@
-export const THEME_COLOR = '#FF6931';
+export const THEME_COLOR = '#10B981';
 export const BUTTON_COLOR = '#000000';
 export const BG_COLOR = '#ffffff';
-export const TEXT_COLOR = '#212121';
+export const TEXT_COLOR = '#064E3B';
 export const WHITE = '#ffffff';
 export const GRAY = '#9e9e9e';
 export const LIGHT_GRAY = '#6e6e6e';
